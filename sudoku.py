f289d47df2e33b73483395c4605193aade3f8ab2
GabrielSDC <gabrielsantosdacosta2@gmail.com>
2024-04-10 23:42:38 -0300

    highlighting cells with the same value as the selectec one

--- sudoku.py
+++ sudoku.py
@@ -6,13 +6,16 @@ size    = width, height = 271, 271
 screen  = pg.display.set_mode(size)
 clock   = pg.time.Clock()
 font    = pg.font.get_default_font()
-bd_font = pg.font.SysFont(font, 26, True, False)
-nm_font = pg.font.SysFont(font, 26, False, False)
+bd_font = pg.font.SysFont(font, 28, True, False)
+nm_font = pg.font.SysFont(font, 28, False, False)
 running = True
 dt      = 0
 
-black = (0,   0,   0)
-white = (255, 255, 255)
+black   = (0,   0,   0  )
+white   = (255, 255, 255)
+blue    = (0,   0,   255)
+lt_blue = (135, 206, 235)
+red     = (255, 0,   0  )
 
 selected = pg.Vector2(0, 0)
 
@@ -32,17 +35,38 @@ for i in range(9):
     for j in range(9):
         aux = {
             "value": 0,
-            "position": (j * 30 + 11, i * 30 + 10),
+            "position": (j * 30 + 11, i * 30 + 7),
             "text": nm_font.render(' ', True, black)
         }
         board[i].append(aux)
 
+conflicting_cells = []
+
+def find_errors(x: int, y: int, value: int) -> bool:
+    for i in range(9):
+        if i != x and board[i][y]["value"] == value:
+            conflicting_cells.append((value, i, y))
+        if i != y and board[x][i]["value"] == value:
+            conflicting_cells.append((value, x, i))
+    
+    section_x, section_y = (x // 3) * 3, (y // 3) * 3
+    for i in range(section_y, section_y + 3):
+        for j in range(section_x, section_x + 3):
+            if i != y and j != x and board[i][j]["value"] == value:
+                conflicting_cells.append((value, i, j))
+
 def update_cell(x: int, y: int, value: int):
-    board[y][x]["value"] = value
     if value == 0:
         board[y][x]["text"] = nm_font.render(' ', True, black)
     else:
         board[y][x]["text"] = nm_font.render(str(value), True, black)
+        find_errors(x, y, value)
+
+    board[y][x]["value"] = value
+
+def draw_colored_square(x: int, y: int, color: tuple):
+    selected_points = [(x, y), (x + 30, y), (x + 30, y + 30), (x, y + 30)]
+    pg.draw.polygon(screen, color, selected_points, 0)
 
 while running:
     for event in pg.event.get():
@@ -54,8 +78,8 @@ while running:
                 update_cell(x, y, 0)
             if event.key > pg.K_0 and event.key <= pg.K_9:
                 update_cell(x, y, event.key - pg.K_0)
-            if event.key > pg.K_KP0 and event.key <= pg.K_KP9:
-                update_cell(x, y, event.key - pg.K_KP0)
+            if event.key >= pg.K_KP1 and event.key <= pg.K_KP9:
+                update_cell(x, y, event.key - pg.K_KP1 + 1)
 
             if event.key == pg.K_UP or event.key == pg.K_w:
                 selected.y = max(0, selected.y - 30)
@@ -73,8 +97,15 @@ while running:
         selected.y = position[1] - position[1] % 30
 
     screen.fill(white)
-    selected_points = [(selected.x, selected.y), (selected.x + 30, selected.y), (selected.x + 30, selected.y + 30), (selected.x, selected.y + 30)]
-    pg.draw.polygon(screen, (0, 0, 255), selected_points, 0)
+
+    sec_x, sec_y = int(selected.x // 30), int(selected.y // 30)
+    selected_value = board[sec_y][sec_x]["value"]
+    for i in range(9):
+        for j in range(9):
+            if selected_value and board[i][j]["value"] == selected_value:
+                draw_colored_square(j * 30, i * 30, lt_blue)
+
+    draw_colored_square(selected.x, selected.y, blue)
 
     for line in board:
         for cell in line:
